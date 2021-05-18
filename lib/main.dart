8d31beeed533370209dea203cec6a6cb20a297ee
mrityunjay2627 <mrityunjay2627@gmail.com>
2021-05-18 19:36:16 +0530

    LOGIN PAGE SETUP

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/LandingPage.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Instagram",
+      theme: ThemeData(
+        fontFamily: 'Raleway',
+        primaryColor: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LandingPage(),
+    );
+  }
+}
